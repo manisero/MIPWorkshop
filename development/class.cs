e108945cbd59d0f90f5a4285f9e69e175abc6473
Uladzimir Palkhouski <uladzimir.palkhouski@gmail.com>
2014-02-05 10:46:00 +0100

    commiting changed cass

--- development/class.cs
+++ development/class.cs
@@ -34,25 +34,44 @@ namespace MIP.Infrastructure.IO
 
 		#region IFileSystemService Members
 
+
+		
 		/// <summary>
-		/// 	Determines whether the specified file exists.
+		/// Returns the names of files in the specified directory.
 		/// </summary>
-		/// <param name="filePath"> The file path. </param>
-		/// <returns> </returns>
-		public bool FileExists(string filePath)
+		/// <param name="directoryPath"></param>
+		/// <returns></returns>
+		public string[] GetFiles(String directoryPath)
 		{
-			return File.Exists(filePath);
+			return Directory.GetFiles(directoryPath);
 		}
 
 		/// <summary>
-		/// 	Reads specified file
+		/// Read all bytes from stream
 		/// </summary>
-		/// <param name="filePath"> The file path. </param>
-		/// <returns> </returns>
-		public string ReadFromFile(string filePath)
+		/// <param name="stream"></param>
+		/// <returns></returns>
+		public byte[] ReadAllBytesFromStream(Stream stream)
 		{
-			return File.ReadAllText(filePath);
+			using (var memmoryStream = new MemoryStream())
+			{
+				stream.CopyTo(memmoryStream);
+				return memmoryStream.ToArray();
+			}
 		}
+
+		/// <summary>
+		/// Zips the directory.
+		/// </summary>
+		/// <param name="directoryName">Name of the directory.</param>
+		/// <param name="destinationFilePath">The destination file path.</param>
+		public void ZipDirectory(string directoryName, string destinationFilePath)
+		{
+			new FastZip().CreateZip(destinationFilePath, directoryName, true, string.Empty);
+		}
+
+	
+
 
 		/// <summary>
 		/// 	Reads the bytes from file.
@@ -67,6 +86,27 @@ namespace MIP.Infrastructure.IO
 		}
 
 		/// <summary>
+		/// 	Reads specified file
+		/// </summary>
+		/// <param name="filePath"> The file path. </param>
+		/// <returns> </returns>
+		public string ReadFromFile(string filePath)
+		{
+			return File.ReadAllText(filePath);
+		}
+
+			/// <summary>
+		/// 	Determines whether the specified file exists.
+		/// </summary>
+		/// <param name="filePath"> The file path. </param>
+		/// <returns> </returns>
+		public bool FileExists(string filePath)
+		{
+			return File.Exists(filePath);
+		}
+
+
+		/// <summary>
 		/// 	Writes content to the specified file.
 		/// </summary>
 		/// <param name="filePath"> The file path. </param>
@@ -74,6 +114,29 @@ namespace MIP.Infrastructure.IO
 		public void WriteToFile(string filePath, string content)
 		{
 			File.WriteAllText(filePath, content);
+		}
+
+		
+
+		/// <summary>
+		/// Reads from archive.
+		/// </summary>
+		/// <param name="archivePath">The archive path.</param>
+		/// <returns></returns>
+		public string ReadSingleFileFromArchive(string archivePath)
+		{
+			var archiveStream = GetFileStream(archivePath);
+			return DecompressSingleFileFromStream(archiveStream);
+		}
+
+		/// <summary>
+		/// 	Writes <see cref="byte">byte</see> array to file. (DTC Transaction supported)
+		/// </summary>
+		/// <param name="filePath"> The file path. </param>
+		/// <param name="bytes"> <see cref="byte">Byte</see> array to be written. </param>
+		public void WriteBytesToFile(string filePath, byte[] bytes)
+		{
+			File.WriteAllBytes(filePath, bytes);
 		}
 
 		/// <summary>
@@ -99,27 +162,6 @@ namespace MIP.Infrastructure.IO
 		}
 
 		/// <summary>
-		/// Reads from archive.
-		/// </summary>
-		/// <param name="archivePath">The archive path.</param>
-		/// <returns></returns>
-		public string ReadSingleFileFromArchive(string archivePath)
-		{
-			var archiveStream = GetFileStream(archivePath);
-			return DecompressSingleFileFromStream(archiveStream);
-		}
-
-		/// <summary>
-		/// 	Writes <see cref="byte">byte</see> array to file. (DTC Transaction supported)
-		/// </summary>
-		/// <param name="filePath"> The file path. </param>
-		/// <param name="bytes"> <see cref="byte">Byte</see> array to be written. </param>
-		public void WriteBytesToFile(string filePath, byte[] bytes)
-		{
-			File.WriteAllBytes(filePath, bytes);
-		}
-
-		/// <summary>
 		/// 	Copies the file to stream.
 		/// </summary>
 		/// <param name="outputStream"> The output stream. </param>
@@ -131,6 +173,15 @@ namespace MIP.Infrastructure.IO
 			{
 				s.CopyTo(outputStream);
 			}
+		}
+
+		/// <summary>
+		/// 	Deletes the file.
+		/// </summary>
+		/// <param name="path"> The path. </param>
+		public void DeleteFile(string path)
+		{
+			File.Delete(path);
 		}
 
 		/// <summary>
@@ -156,14 +207,7 @@ namespace MIP.Infrastructure.IO
 			}
 		}
 
-		/// <summary>
-		/// 	Deletes the file.
-		/// </summary>
-		/// <param name="path"> The path. </param>
-		public void DeleteFile(string path)
-		{
-			File.Delete(path);
-		}
+		
 
 		/// <summary>
 		/// Deletes file if file exists in file system.
@@ -177,14 +221,6 @@ namespace MIP.Infrastructure.IO
 			}
 		}
 
-		/// <summary>
-		/// Deletes the folder if exist.
-		/// </summary>
-		/// <param name="folderToDelete">The folder to delete.</param>
-		public void DeleteFolderIfExist(string folderToDelete)
-		{
-			new DirectoryInfo(folderToDelete).Delete(true);
-		}
 
 		/// <summary>
 		/// 	Writes to file specified content
@@ -200,6 +236,28 @@ namespace MIP.Infrastructure.IO
 				content.Seek(0, SeekOrigin.Begin);
 				content.CopyTo(fileStream);
 			}
+		}
+
+
+		/// <summary>
+		/// Deletes the folder if exist.
+		/// </summary>
+		/// <param name="folderToDelete">The folder to delete.</param>
+		public void DeleteFolderIfExist(string folderToDelete)
+		{
+			new DirectoryInfo(folderToDelete).Delete(true);
+		}
+
+/// <summary>
+		/// Copies an existing file to a new file.
+		/// </summary>
+		/// <param name="sourceFileName"> The File to copy. </param>
+		/// <param name="destFileName"> The destination File </param>
+		/// <param name="overwrite">true if the destination file can be overwritten; otherwise, false.</param>
+		public void CopyFile(String sourceFileName, String destFileName, bool overwrite = false)
+		{
+			CreateDirectoryIfNotExists(Path.GetDirectoryName(destFileName));
+			File.Copy(sourceFileName, destFileName, overwrite);
 		}
 
 		/// <summary>
@@ -225,28 +283,8 @@ namespace MIP.Infrastructure.IO
 			}
 		}
 
-		/// <summary>
-		/// Copies an existing file to a new file.
-		/// </summary>
-		/// <param name="sourceFileName"> The File to copy. </param>
-		/// <param name="destFileName"> The destination File </param>
-		/// <param name="overwrite">true if the destination file can be overwritten; otherwise, false.</param>
-		public void CopyFile(String sourceFileName, String destFileName, bool overwrite = false)
-		{
-			CreateDirectoryIfNotExists(Path.GetDirectoryName(destFileName));
-			File.Copy(sourceFileName, destFileName, overwrite);
-		}
+		
 
-		/// <summary>
-		/// Moves the file.
-		/// </summary>
-		/// <param name="sourceFileName">Name of the source file.</param>
-		/// <param name="destFileName">Name of the dest file.</param>
-		public void MoveFile(string sourceFileName, string destFileName)
-		{
-			CreateDirectoryIfNotExists(Path.GetDirectoryName(destFileName));
-			File.Move(sourceFileName, destFileName);
-		}
 
 		/// <summary>
 		/// 	Determines whether the given path refers to an existing directory on disk.
@@ -268,131 +306,19 @@ namespace MIP.Infrastructure.IO
 			return Directory.CreateDirectory(directoryPath);
 		}
 
+
 		/// <summary>
-		/// 	Creates all directories and subdirectories as specified by path.
+		/// Moves the file.
 		/// </summary>
-		/// <param name="directoryPath"> The directory path. </param>
-		/// <returns> </returns>
-		public DirectoryInfo CreateDirectoryIfNotExists(string directoryPath)
+		/// <param name="sourceFileName">Name of the source file.</param>
+		/// <param name="destFileName">Name of the dest file.</param>
+		public void MoveFile(string sourceFileName, string destFileName)
 		{
-			var directoryInfo = new DirectoryInfo(directoryPath);
-
-			if (!DirectoryExists(directoryPath))
-			{
-				CreateDirectory(directoryPath);
-			}
-
-			return directoryInfo;
+			CreateDirectoryIfNotExists(Path.GetDirectoryName(destFileName));
+			File.Move(sourceFileName, destFileName);
 		}
 
-		/// <summary>
-		/// 	Makes backup file
-		/// </summary>
-		/// <param name="filePath"> The current file path. </param>
-		public void BackUpFile(string filePath)
-		{
-			var currentDirectory = Path.GetDirectoryName(filePath);
-			if (currentDirectory == null)
-			{
-				throw new DirectoryNotFoundException("Cannot find directory.");
-			}
-
-			var backUpFileName = String.Format("{0}{1}{2}", Path.GetFileNameWithoutExtension(filePath),
-											   DateTime.UtcNow.ToString(BACKUP_FILE_NAME_FORMAT),
-											   Path.GetExtension(filePath));
-			var backupFilePath = Path.Combine(currentDirectory, backUpFileName);
-			if (FileExists(backupFilePath))
-			{
-				throw new InvalidDataException("BackUp file already exists: " + backupFilePath);
-			}
-
-			CopyFile(filePath, backupFilePath);
-		}
-
-		/// <summary>
-		/// 	Creates File
-		/// </summary>
-		/// <param name="filePath"> The current file path. </param>
-		public FileStream CreateFile(string filePath)
-		{
-			return File.Create(filePath);
-		}
-
-		/// <summary>
-		/// 	Gets filestream by file path
-		/// </summary>
-		/// <param name="filePath"> The current file path. </param>
-		/// <returns> </returns>
-		public Stream GetFileStream(string filePath)
-		{
-			return new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
-		}
-
-		/// <summary>
-		/// Compress files
-		/// </summary>
-		/// <param name="files">The files that will be included to the compress file.</param>
-		/// <param name="destinationFilePath">The path to the compress file that will be created</param>
-		public void CompressFiles(IDictionary<string, byte[]> files, string destinationFilePath)
-		{
-			using (var outStream = new FileStream(destinationFilePath, FileMode.OpenOrCreate))
-			{
-				using (var zipStream = new ZipOutputStream(outStream))
-				{
-					zipStream.SetLevel(COMPRESSION_LEVEL);
-
-					foreach (var embeddedFile in files)
-					{
-						var zipEntity = new ZipEntry(embeddedFile.Key)
-							{
-								DateTime = DateTime.UtcNow,
-								Size = embeddedFile.Value.Length
-							};
-
-						zipStream.PutNextEntry(zipEntity);
-
-						var buffer = new byte[BUFFER_SIZE];
-						using (var embeddedFileStream = new MemoryStream(embeddedFile.Value))
-						{
-							StreamUtils.Copy(embeddedFileStream, zipStream, buffer);
-						}
-					}
-
-					zipStream.CloseEntry();
-
-					zipStream.IsStreamOwner = true;
-				}
-			}
-		}
-
-		/// <summary>
-		/// Decompresses the input stream.
-		/// </summary>
-		/// <param name="inputStream">The archive stream.</param>
-		/// <returns></returns>
-		public Dictionary<string, Stream> DecompressStream(Stream inputStream)
-		{
-			var result = new Dictionary<String, Stream>();
-
-			using (var zipInputStream = new ZipInputStream(inputStream))
-			{
-				ZipEntry zipEntry = zipInputStream.GetNextEntry();
-				while (zipEntry != null)
-				{
-					var buffer = new byte[BUFFER_SIZE];
-					var stream = new MemoryStream((int)zipEntry.Size);
-
-					StreamUtils.Copy(zipInputStream, stream, buffer);
-					result.Add(zipEntry.Name, stream);
-
-					zipEntry = zipInputStream.GetNextEntry();
-				}
-			}
-
-			return result;
-		}
-
-		/// <summary>
+	/// <summary>
 		/// Decompresses the file.
 		/// </summary>
 		/// <param name="filePath">The file path.</param>
@@ -486,6 +412,139 @@ namespace MIP.Infrastructure.IO
 			outStream.Position = 0;
 			return outStream;
 		}
+		
+
+		/// <summary>
+		/// 	Makes backup file
+		/// </summary>
+		/// <param name="filePath"> The current file path. </param>
+		public void BackUpFile(string filePath)
+		{
+			var currentDirectory = Path.GetDirectoryName(filePath);
+			if (currentDirectory == null)
+			{
+				throw new DirectoryNotFoundException("Cannot find directory.");
+			}
+
+			var backUpFileName = String.Format("{0}{1}{2}", Path.GetFileNameWithoutExtension(filePath),
+											   DateTime.UtcNow.ToString(BACKUP_FILE_NAME_FORMAT),
+											   Path.GetExtension(filePath));
+			var backupFilePath = Path.Combine(currentDirectory, backUpFileName);
+			if (FileExists(backupFilePath))
+			{
+				throw new InvalidDataException("BackUp file already exists: " + backupFilePath);
+			}
+
+			CopyFile(filePath, backupFilePath);
+		}
+
+		/// <summary>
+		/// 	Creates all directories and subdirectories as specified by path.
+		/// </summary>
+		/// <param name="directoryPath"> The directory path. </param>
+		/// <returns> </returns>
+		public DirectoryInfo CreateDirectoryIfNotExists(string directoryPath)
+		{
+			var directoryInfo = new DirectoryInfo(directoryPath);
+
+			if (!DirectoryExists(directoryPath))
+			{
+				CreateDirectory(directoryPath);
+			}
+
+			return directoryInfo;
+		}
+
+
+		/// <summary>
+		/// 	Creates File
+		/// </summary>
+		/// <param name="filePath"> The current file path. </param>
+		public FileStream CreateFile(string filePath)
+		{
+			return File.Create(filePath);
+		}
+
+	
+
+		/// <summary>
+		/// Decompresses the input stream.
+		/// </summary>
+		/// <param name="inputStream">The archive stream.</param>
+		/// <returns></returns>
+		public Dictionary<string, Stream> DecompressStream(Stream inputStream)
+		{
+			var result = new Dictionary<String, Stream>();
+
+			using (var zipInputStream = new ZipInputStream(inputStream))
+			{
+				ZipEntry zipEntry = zipInputStream.GetNextEntry();
+				while (zipEntry != null)
+				{
+					var buffer = new byte[BUFFER_SIZE];
+					var stream = new MemoryStream((int)zipEntry.Size);
+
+					StreamUtils.Copy(zipInputStream, stream, buffer);
+					result.Add(zipEntry.Name, stream);
+
+					zipEntry = zipInputStream.GetNextEntry();
+				}
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// 	Gets filestream by file path
+		/// </summary>
+		/// <param name="filePath"> The current file path. </param>
+		/// <returns> </returns>
+		public Stream GetFileStream(string filePath)
+		{
+			return new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+		}
+
+
+
+
+		/// <summary>
+		/// Compress files
+		/// </summary>
+		/// <param name="files">The files that will be included to the compress file.</param>
+		/// <param name="destinationFilePath">The path to the compress file that will be created</param>
+		public void CompressFiles(IDictionary<string, byte[]> files, string destinationFilePath)
+		{
+			using (var outStream = new FileStream(destinationFilePath, FileMode.OpenOrCreate))
+			{
+				using (var zipStream = new ZipOutputStream(outStream))
+				{
+					zipStream.SetLevel(COMPRESSION_LEVEL);
+
+					foreach (var embeddedFile in files)
+					{
+						var zipEntity = new ZipEntry(embeddedFile.Key)
+							{
+								DateTime = DateTime.UtcNow,
+								Size = embeddedFile.Value.Length
+							};
+
+						zipStream.PutNextEntry(zipEntity);
+
+						var buffer = new byte[BUFFER_SIZE];
+						using (var embeddedFileStream = new MemoryStream(embeddedFile.Value))
+						{
+							StreamUtils.Copy(embeddedFileStream, zipStream, buffer);
+						}
+					}
+
+					zipStream.CloseEntry();
+
+					zipStream.IsStreamOwner = true;
+				}
+			}
+		}
+
+	
 
 		/// <summary>
 		/// Compress File
@@ -521,39 +580,6 @@ namespace MIP.Infrastructure.IO
 			}
 		}
 
-		/// <summary>
-		/// Returns the names of files in the specified directory.
-		/// </summary>
-		/// <param name="directoryPath"></param>
-		/// <returns></returns>
-		public string[] GetFiles(String directoryPath)
-		{
-			return Directory.GetFiles(directoryPath);
-		}
-
-		/// <summary>
-		/// Read all bytes from stream
-		/// </summary>
-		/// <param name="stream"></param>
-		/// <returns></returns>
-		public byte[] ReadAllBytesFromStream(Stream stream)
-		{
-			using (var memmoryStream = new MemoryStream())
-			{
-				stream.CopyTo(memmoryStream);
-				return memmoryStream.ToArray();
-			}
-		}
-
-		/// <summary>
-		/// Zips the directory.
-		/// </summary>
-		/// <param name="directoryName">Name of the directory.</param>
-		/// <param name="destinationFilePath">The destination file path.</param>
-		public void ZipDirectory(string directoryName, string destinationFilePath)
-		{
-			new FastZip().CreateZip(destinationFilePath, directoryName, true, string.Empty);
-		}
 
 		#endregion
 	}
